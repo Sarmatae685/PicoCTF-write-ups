35be54e8fe22430c38c6259320e70eea68d57254
sarmatae <doc561933@gmail.com>
2025-11-25 12:43:13 +0200

    Modified quartz/i18n/locales/en-US.ts: 'with' -> 'by'

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -36,7 +36,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Created with",
+      createdWith: "Created by", // Created with --> by
     },
     graph: {
       title: "Graph View",
